87873de115e1ff7016b43c552690423d6aee4393
Joseph DiPeri <jdiperi88@gmail.com>
2017-08-01 00:46:19 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const session = require('express-session');
+const passport = require('passport');
+
+const app = express();
+require('dotenv').config();
